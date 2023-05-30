de0ad381f75bc832e03dbf5a8acb3333732b2098
poyu9090 <33018017+poyu9090@users.noreply.github.com>
2023-05-31 00:04:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -141,8 +141,9 @@ def search_post(user_id):
     cursor = conn.cursor()
 
     # 创建包含关键字的查询语句
-    placeholders = " OR post_content LIKE CONCAT('%', %s, '%') " * len(keywords)
-    sql = f"SELECT post_content, post_time, post_link FROM post_data WHERE {placeholders}"
+    placeholders = " OR post_content LIKE CONCAT('%', %s, '%') "
+    condition = " OR ".join([placeholders] * len(keywords))
+    sql = f"SELECT post_content, post_time, post_link FROM post_data WHERE {condition}"
     sql += "AND post_time >= DATE_SUB(NOW(), INTERVAL 1 MONTH) ORDER BY post_time DESC"
 
     # 執行 SQL 查詢
